5cc865ced7ef026fb517c991de6b26ab83de11b4
David Jones <drj@pobox.com>
2015-09-03 11:41:16 +0100

    Store Easting and Northing in data table

--- scraper.py
+++ scraper.py
@@ -6,13 +6,25 @@ from __future__ import print_function
 # import scraperwiki
 # import lxml.html
 
+import re
+
 import requests
+import scraperwiki
 
 
 def main():
+    bus = "505"
     r = requests.get('http://maps.travelsouthyorkshire.com/RouteWkt/505_LIN_1_2.wkt')
 
     print(r.text)
 
-print(__name__)
-main()
+    for i,m in enumerate(re.findall(r'(\d+) +(\d+)', r.text)):
+        easting, northing = map(int, m)
+        print(bus, i, easting, northing)
+        scraperwiki.sqlite.save(unique_keys=['bus', 'point_index'],
+          data=dict(bus=bus,
+            point_index=i, easting=easting, northing=northing))
+
+
+if __name__ == '__main__':
+    main()
