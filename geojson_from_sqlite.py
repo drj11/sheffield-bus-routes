c024520c38b6705b71b3f59f488776f5bd1cbaea
David Jones <drj@pobox.com>
2015-09-09 14:03:52 +0100

    Fix the .geojson to be proper geojson

--- geojson_from_sqlite.py
+++ geojson_from_sqlite.py
@@ -13,25 +13,35 @@ def main():
     cursor.execute(
       "SELECT route, line_index, point_index, easting, northing FROM swdata")
 
-    # :todo: one set of lines per route, not one set for all routes
-    geojson_lines = []
+    # One feature per route, each feature being a MultiLineString.
+    features = []
     for route, lines in itertools.groupby(cursor, lambda r: r[0]):
+        # Will be the coordinate member of a MultiLineString.
+        # 3D-list.
+        geojson_route = []
         print("============ ROUTE", route)
         for line, points in itertools.groupby(lines, lambda r: r[1]):
             print("========= LINE", line)
-            geojson_line = dict(type='LineString', coordinates=[])
+            line_string = []
             for row in points:
                 route, line_index, point_index, easting, northing = row
                 p = latlon.osgrid_to_wgs84((easting, northing))
                 lat = p.latitude
                 lon = p.longitude
-                geojson_line['coordinates'].append([lon, lat])
-            print(geojson_line)
-            geojson_lines.append(geojson_line)
+                line_string.append([lon, lat])
+            print(line_string)
+            geojson_route.append(line_string)
+        feature = dict(type="Feature",
+          properties=None,
+          geometry=dict(type="MultiLineString",
+          coordinates=geojson_route))
+        features.append(feature)
+    geojson_collection = dict(type='FeatureCollection',
+      features=features)
 
     with open("routes.geojson", 'w') as w:
         # w.write("var routes = ")
-        json.dump(geojson_lines, w, indent=2)
+        json.dump(geojson_collection, w, indent=2)
 
 
 if __name__ == '__main__':
